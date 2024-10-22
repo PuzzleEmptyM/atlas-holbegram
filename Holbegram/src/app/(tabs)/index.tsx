6057e4946007e7e961bb58a6339787142ea86936
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-10-22 16:00:25 -0500

    some test styling

--- Holbegram/src/app/(tabs)/index.tsx
+++ Holbegram/src/app/(tabs)/index.tsx
@@ -1,5 +1,12 @@
 import { Text } from "react-native";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
 
 export default function FeedScreen() {
-  return <Text>Feed</Text>;
+  return (
+    <View className="bg-slate-300 flex-1 items-center justify-center m-10">
+      <Text className="text-md font-bold text-blue-300">
+        Feed
+      </Text>
+    </View>
+  );
 }
